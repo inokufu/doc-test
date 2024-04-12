afe3d35cebd01ea96fb778041a0c9f408e95fb5f
guilfb <guillaumelefebvre69@gmail.com>
2024-04-12 14:43:49 +0000

    Added a check to see if settings are all set to access the DB

--- app/main.py
+++ app/main.py
@@ -7,18 +7,23 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from app.config import settings
 from app.models.item import Item
 from app.routers.item import item_router
-from app.utils.log import init_logging, logger
+from app.utils.log import init_logging
 
 init_logging()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    logger.info("Application starting up")
+    # Check if MongoDB URL and MongoDB Database Name are provided
+    if settings.mongodb_url is None or settings.mongodb_name is None:
+        raise ValueError(
+            "MongoDB URL and MongoDB Database Name must be provided in the environment variables"
+        )
+
+    # Connect to MongoDB and initialize Beanie ODM
     client = AsyncIOMotorClient(settings.mongodb_url)
     await init_beanie(client[settings.mongodb_name], document_models=[Item])
     yield
-    logger.info("Application shutting down")
 
 
 app = FastAPI(lifespan=lifespan)
