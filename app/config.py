bd936aed08f71861310c4a4653ab46df1f8647ce
guilfb <guillaumelefebvre69@gmail.com>
2024-04-12 14:43:18 +0000

    Added settings description

--- app/config.py
+++ app/config.py
@@ -1,9 +1,16 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    mongodb_url: str
-    mongodb_name: str
+    mongodb_url: str | None = Field(
+        default=None, title="MongoDB URL", description="MongoDB URL with credentials"
+    )
+    mongodb_name: str | None = Field(
+        default=None,
+        title="MongoDB Database Name",
+        description="MongoDB Database Name to connect to",
+    )
 
     class Config:
         env_file = ".env"
